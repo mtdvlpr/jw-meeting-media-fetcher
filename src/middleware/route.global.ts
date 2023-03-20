4816d50cf04c6206caa74416024e756a9a13543d
Manoah <149895ja@gmail.com>
2023-03-20 17:26:48 +0100

    fix: middleware

--- src/middleware/route.global.ts
+++ src/middleware/route.global.ts
@@ -2,15 +2,16 @@ export default defineNuxtRouteMiddleware((to, from) => {
   log.debug('from', from)
   log.debug('to', to)
   const ignoreList = ['/', '/media', '/browser']
-  if (!ignoreList.includes(to.path)) {
-    if (from.fullPath === to.fullPath) {
-      log.debug('Redirecting to /')
-      return navigateTo({ path: '/', query: { cong: undefined } })
-    } else if (from.query.cong && !to.query.cong) {
-      to.query.cong = from.query.cong
-    } else if (!from.query.cong && !to.query.cong) {
-      log.debug('Redirecting to /')
-      return navigateTo('/')
-    }
+  const ignore = ignoreList.includes(to.path)
+
+  // Redirect on hard refresh
+  if (!ignore && from.fullPath === to.fullPath) {
+    log.debug('Redirecting to /')
+    return navigateTo({ path: '/', query: { cong: undefined } })
+  }
+
+  // Add cong if missing
+  if (from.query.cong && !to.query.cong) {
+    to.query.cong = from.query.cong
   }
 })
