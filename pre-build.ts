5caa5c020b84b475e30ab724ed5a67f8effec6ce
Manoah <149895ja@gmail.com>
2023-04-02 11:53:49 +0200

    refactor: convert prbuild to ts

--- pre-build.ts
+++ pre-build.ts
@@ -1,7 +1,12 @@
-#!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
-
+const path = require('path')
 const fs = require('fs-extra')
+const fluentFfmpegPath = path.join(
+  __dirname,
+  'node_modules',
+  'fluent-ffmpeg',
+  'lib'
+)
 
 // Fix for: https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/573
 fs.copySync(fluentFfmpegPath, fluentFfmpegPath + '-cov')
