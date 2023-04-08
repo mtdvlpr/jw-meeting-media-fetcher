2f2ef29c053867e3d95f728f142b43f51fd92d3d
Manoah <149895ja@gmail.com>
2023-04-08 09:29:11 +0200

    fix: correctly scroll to top

--- src/components/media/MediaControls.vue
+++ src/components/media/MediaControls.vue
@@ -52,8 +52,8 @@ import { LocalFile } from '~~/types'
 
 const loading = ref(true)
 const addSong = ref(false)
-watch(addSong, (val) => {
-  if (val) window.scrollTo(0, 0)
+watch(addSong, () => {
+  scrollToItem(0)
 })
 
 // Current meeting date
