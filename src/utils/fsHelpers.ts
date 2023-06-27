40e2784cbf83c48749ea6ebfc19292457585cf32
Manoah Tervoort <46671786+mtdvlpr@users.noreply.github.com>
2023-06-27 07:13:19 +0200

    feat: support m4v files

--- src/utils/fsHelpers.ts
+++ src/utils/fsHelpers.ts
@@ -6,7 +6,7 @@ function isOneOf(file: string, exts: string[]) {
 }
 
 export function isVideo(file: string) {
-  return isOneOf(file, ['mov', 'mp4', 'mpeg', 'mpg', 'ogg', 'ogv', 'webm'])
+  return isOneOf(file, ['mov', 'mp4', 'm4v', 'mpeg', 'mpg', 'ogg', 'ogv', 'webm'])
 }
 
 export function isImage(file: string) {
